97f72ccce2aaa285d647e05aca1a34fb109a075e
Tomass <blanks.tomass@yahoo.lv>
2020-08-19 20:07:59 +0300

    added dependency injection, Iconfiguration and Serilog

--- WebShopScraper/WebShopScraper/Program.cs
+++ WebShopScraper/WebShopScraper/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
 
 namespace WebShopScraper
 {
@@ -6,7 +10,23 @@ namespace WebShopScraper
     {
         static void Main(string[] args)
         {
-           
+            var builder = new ConfigurationBuilder();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var host = Host.CreateDefaultBuilder()
+                 .ConfigureServices((context, services) =>
+                 {
+
+                 })
+                 .UseSerilog()
+                 .Build();
+
+            var controller = ActivatorUtilities.CreateInstance<Controller>(host.Services);
         }
     }
 }
---
+++ WebShopScraper/WebShopScraper/WebClient.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShopScraper
+{
+    public class WebClient
+    {
+        private readonly IRestClient _client;
+        private readonly ILogger<WebClient> _log;
+        private readonly IConfiguration _config;
+        public WebClient(IRestClient client, ILogger<WebClient> log, IConfiguration config)
+        {
+            _client = client;
+            _log = log;
+            _config = config;
+        }
+        public void GetPageHtmlContent()
+        {
+            
+        }
+
+    }
+}
